d8d79ab31799545294c4266c5053c0981f97c6ed
tialla-kka <tialla99@hanmail.net>
2020-10-22 18:08:59 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCz0HQdKmb2DDSjE_RuhnPnAiEofy53D58", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
