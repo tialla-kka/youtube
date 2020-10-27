0eed49f7faab67d4143cbd544722890302d5ff07
tialla-kka <tialla99@hanmail.net>
2020-10-27 17:20:33 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -15,11 +15,12 @@ function App() {
     
     fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=", requestOptions) 
       .then(response => response.json())
-      .then(result => setVideos(result.items))
+      .then(result => setVideos(result.items))    
       .catch(error => console.log('error', error));
   },[]);
 
   return <VideoList videos={videos} />;
+  
 }
 
 export default App;
