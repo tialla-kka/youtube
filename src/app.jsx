faf8208f439bdf0e4fb5615a647419f063b6d6ac
tialla-kka <tialla99@hanmail.net>
2020-10-28 21:06:30 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import styles from './app.module.css';
-import SearchHeader from './components/search_header/search_header';
-import VideoList from './components/video_list/video_list';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
+import VideoList from "./components/video_list/video_list";
 
-function App({youtube}) {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const search = query =>{
-    youtube
-      .search(query)
-      .then(videos => setVideos(videos));      
-  }
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(()=>{
-    youtube
-      .mostPopular()
-      .then(videos => setVideos(videos));
-  },[]);
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+  const search = (query) => {
+    setSelectedVideo(null);
+    youtube.search(query).then((videos) => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube.mostPopular().then((videos) => setVideos(videos));
+  }, []);
 
   return (
-  <div className={styles.app}>
-    <SearchHeader onSearch={search}/>
-    <VideoList videos={videos} />;
-  </div>
-  )
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+          ;
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
